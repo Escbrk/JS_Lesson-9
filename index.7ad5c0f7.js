c71cb7be2402cad41ab72256da5192b7a2fe9ae9
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-27 04:37:46 +0000

    Deploying to gh-pages from @ Escbrk/JS_Lesson-9@035993885db033b1cfb3bc1133e73ee6cb34b154 🚀

---
+++ index.7ad5c0f7.js
@@ -0,0 +1,4 @@
+//! setTimeout()
+//! setInterval()
+const i={notification:document.querySelector(".js-alert")};i.notification.addEventListener("click",(function(){i.notification.classList.remove("is-visible")})),i.notification.classList.add("is-visible"),setTimeout((()=>{i.notification.classList.remove("is-visible")}),3e3);
+//# sourceMappingURL=index.7ad5c0f7.js.map
---
+++ index.837dd45c.js
@@ -0,0 +1,5 @@
+!function(){
+//! setTimeout()
+//! setInterval()
+var i={notification:document.querySelector(".js-alert")};i.notification.addEventListener("click",(function(){i.notification.classList.remove("is-visible")})),i.notification.classList.add("is-visible"),setTimeout((function(){i.notification.classList.remove("is-visible")}),3e3)}();
+//# sourceMappingURL=index.837dd45c.js.map
