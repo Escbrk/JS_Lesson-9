953b2cc76255fd2b23eaed0566ba23e2b8f2b6ad
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-26 21:26:01 -0700

    'upd'

--- src/index.js
+++ src/index.js
@@ -1,130 +1,171 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import axios from 'axios';
+// const myUser = {
+//   name: 'Max',
+//   age: 23,
+//   phoneNumber: 654654654,
+// };
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// const name = `{"name": "Mango"}`;
+
+// const str = JSON.stringify(myUser);
+// console.log(str);
+// console.log(JSON.parse(str));
+// console.log(JSON.parse(name));
+
+// const user = {
+//   name: 'Paul',
+//   age: 27,
+//   city: 'Calgary',
+// };
+
+// localStorage.setItem('data', JSON.stringify(user))
+
+// const result = JSON.parse(localStorage.getItem('data'))
+// console.log(result)
+
+// try {
+//   const result = JSON.parse(localStorage.getItem('key1'));
+//   console.log(result);
+// } catch {
+//   console.log('Error');
+// }
+
+//?_____________________________________________
+// import throttle from 'lodash.throttle';
+// import { saveToLS, loadFromLS } from '../helpers/helpers';
+
+// //! ВАРИАНТ БЕЗ ОБЪЕКТА
+// // const refs = {
+// //   form: document.querySelector('.js-feedback-form'),
+// // };
+// // afterReload();
+
+// // refs.form.addEventListener('input', throttle(onInput, 500));
+// // refs.form.addEventListener('submit', onSubmit);
+
+// // function onInput(e) {
+// //   const key = e.target.name;
+// //   const value = e.target.value;
+
+// //   saveToLS(key, value);
+// // }
+
+// // function afterReload() {
+// //   const name = loadFromLS('name');
+// //   const message = loadFromLS('message');
+
+// //   refs.form.elements.name.value = name || 'Anonymous';
+// //   refs.form.elements.message.value = message;
+// // }
+
+// // function onSubmit(e) {
+// //   e.preventDefault();
+// //   const name = refs.form.elements.name.value;
+// //   const message = refs.form.elements.message.value;
+
+// //   const obj = {
+// //     name,
+// //     message,
+// //   };
+
+// //   console.log(obj);
+
+// //   e.target.reset();
+// //   localStorage.removeItem('name');
+// //   localStorage.removeItem('message');
+// // }
+
+// //! ВАРИАНТ С ОБЪЕКТОМ
+// const refs = {
+//   form: document.querySelector('.js-feedback-form'),
+// };
+
+// refs.form.addEventListener('input', throttle(onInput, 500));
+// refs.form.addEventListener('submit', onSubmit);
+
+// function onInput() {
+//   const name = refs.form.elements.name.value;
+//   const message = refs.form.elements.message.value;
+
+//   const obj = {
+//     name,
+//     message,
+//   };
+//   saveToLS('formData', obj);
+// }
+
+// function afterReload() {
+//   const data = loadFromLS('formData');
+
+//   if (data) {
+//     refs.form.elements.name.value = data.name;
+//     refs.form.elements.message.value = data.message;
+//   } else {
+//     refs.form.elements.name.value = 'Anonymous';
+//   }
+// }
+
+// function onSubmit(e) {
+//   e.preventDefault();
+//   console.log(JSON.parse(localStorage.getItem('formData')));
+
+//   e.target.reset();
+//   localStorage.removeItem('formData');
+// }
+// afterReload();
+
+//?_____________________________________________
+
+//! setTimeout()
+// const logger = time =>
+//   console.log(`Лог через ${time}ms, потому что не отменили timeout`);
+
+// const timerId = setTimeout(logger, 2000, 2000);
+
+// const shouldCancelTimer = Math.random() > 0.3;
+// console.log(shouldCancelTimer)
+
+// if (shouldCancelTimer) {
+//     clearTimeout(timerId);
+// }
+
+//! setInterval()
+// const logger = time => console.log(`Лог каждые ${time}ms - ${Date.now()}`);
+
+// console.log('До вызова setInterval');
+// const intervalId = setInterval(logger, 2000, 2000);
+// console.log('После вызова setInterval');
+
+// const shouldCancelInterval = Math.random() > 0.3;
+// console.log(shouldCancelInterval)
+// if (shouldCancelInterval) {
+//   clearInterval(intervalId);
+// }
+
+//?_____________________________________________
+
+// console.log('До вызова setTimeout');
+// setTimeout(() => {
+//     console.log('Вызов отложенной функции')
+// }, 0)
+// console.log('После вызова setTimeout');
+
+//?_____________________________________________
+//* ОПОВЕЩЕНИЯ
 
 const refs = {
-  input: document.querySelector('[type="text"]'),
-  searchBtn: document.querySelector('[type="submit"]'),
-  gallery: document.querySelector('.gallery'),
-  loadBtn: document.querySelector('.js-load'),
-  loadDiv: document.querySelector('.js-loader'),
+  notification: document.querySelector('.js-alert'),
 };
 
-const params = {
-  API_KEY: '41227446-81114c3a771220f4777577230',
-};
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+refs.notification.addEventListener('click', onClick)
 
-let currentPage = 1;
-const per_page = 40;
-let lightbox = new SimpleLightbox('.gallery a');
-
-async function getGaleryItems(page = currentPage) {
-  const data = await axios
-    .get(
-      `?key=${params.API_KEY}&q=${refs.input.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
-    )
-    .then(resp => resp.data)
-    .catch(err => console.error(err));
-  return data;
+function onClick() {
+    //
 }
 
-refs.searchBtn.addEventListener('click', onSearch);
-refs.loadBtn.addEventListener('click', onClick);
-
-function onSearch(e) {
-  e.preventDefault();
-  currentPage = 1;
-
-  if (refs.input.value.trim() === '') {
-    return iziToast.error({
-      message: 'Sorry, the field must be filled in!',
-      position: 'topRight',
-    });
-  } else {
-    refs.gallery.innerHTML = '<span class="loader"></span>';
-  }
-  getGaleryItems()
-    .then(data => {
-      refs.gallery.innerHTML = createMarkup(data.hits);
-      lightbox.refresh();
-
-      if (per_page >= data.totalHits) {
-        refs.loadBtn.hidden = true;
-      } else {
-        refs.loadBtn.hidden = false;
-      }
-    })
-    .catch(err => console.error(err));
+function showNotification() {
+    refs.notification.classList.add('is-visible')
 }
 
-function onClick(e) {
-  e.preventDefault();
-  currentPage += 1;
-
-  refs.loadDiv.innerHTML = '<span class="loader"></span>';
-  refs.loadBtn.hidden = true;
-
-  getGaleryItems(currentPage).then(data => {
-    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-    lightbox.refresh();
-
-    if (data.totalHits / per_page <= currentPage) {
-      refs.loadBtn.hidden = true;
-      refs.loadDiv.innerHTML = '';
-      return iziToast.info({
-        message: "We're sorry, but you've reached the end of search results.",
-        position: 'topRight',
-      });
-    } else if (data.totalHits / per_page >= currentPage) {
-      refs.loadDiv.innerHTML = '';
-      refs.loadBtn.hidden = false;
-    }
-
-    const galeryItem = document.querySelector('.gallery-item');
-    let rect = galeryItem.getBoundingClientRect();
-    scrollBy({
-      top: rect.height * 3,
-      behavior: 'smooth',
-    });
-  });
-}
-
-function createMarkup(arr) {
-  if (arr.length === 0) {
-    iziToast.error({
-      message:
-        'Sorry, there are no images matching <br> your search query. Please try again!',
-      position: 'topRight',
-    });
-  }
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-    <li class='gallery-item'>
-        <a href='${largeImageURL}'>
-            <img src="${webformatURL}" alt="${tags}" data-source=${largeImageURL} class='gallery-img'/>
-        </a>
-        <div class='text-container'>
-            <p>Likes<span>${likes}</span></p>
-            <p>Views<span>${views}</span></p>
-            <p>Comments<span>${comments}</span></p>
-            <p>Downloads<span>${downloads}</span></p>
-        </div>
-    </li>`;
-      }
-    )
-    .join('');
+function hideNotification() {
+    refs.notification.classList.remove('is-visible');
 }
